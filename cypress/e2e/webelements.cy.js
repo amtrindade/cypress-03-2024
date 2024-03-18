9d72613467a5dcd0a0cad7095ae9df8d3367a69f
Antonio Trindade <atrindade@umov.me>
2024-03-18 19:37:31 -0300

    Add tests textfield

--- cypress/e2e/webelements.cy.js
+++ cypress/e2e/webelements.cy.js
@@ -1,6 +1,6 @@
 ///<reference types="cypress"/>
 
-describe("Work with webelements", () => {
+describe("Work with Web elements", () => {
 
     beforeEach(()=> {
         cy.visit("/elementsweb.html");
@@ -14,5 +14,22 @@ describe("Work with webelements", () => {
         cy.get("input[name='txtbox1']")
             .type("Hello world")
             .should("have.value", "Hello world")
+    })
+
+    it("Should be validate enable/disable TextFields", () => {
+        cy.get("input[name='txtbox1']")
+            .should("be.enabled");
+
+        cy.get("input[name='txtbox2']")
+            .should("be.disabled");
+            //.should("not.be.enabled");
+
+    })
+
+    it("Should be validate Text in textfield", () => {
+        cy.get("input[name='txtbox2']")
+            .type("Curso Cypress", {force: true})
+            .should("have.value", "Curso Cypress");
+
     })
 })

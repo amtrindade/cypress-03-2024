54677c51a1f415c88324f4c8538fc8c498e6c5c0
Antonio Trindade <atrindade@umov.me>
2024-03-18 22:02:59 -0300

    Add todo

---
+++ cypress/e2e/todo.cy.js
@@ -0,0 +1,23 @@
+///<reference types="cypress"/>
+
+describe("To Do tests", () => {
+
+    beforeEach(() => {
+        cy.visit("https://example.cypress.io/todo#/");
+    })
+
+    it("Should be todo new task", () => {
+        const task = "Fix the bug";
+
+        cy.get('[data-test="new-todo"]')
+            .type(`${task}{enter}`);
+
+        cy.get("ul[class='todo-list'] li")
+            .should("have.length", 3)
+            .last()
+            .should("have.text", `${task}`);
+
+    })
+
+
+})

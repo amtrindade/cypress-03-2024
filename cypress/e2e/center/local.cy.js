73f70c10faf193623af9421795d335216e56637a
Antonio Trindade <atrindade@umov.me>
2024-03-26 20:09:46 -0300

    Change pass

--- cypress/e2e/center/local.cy.js
+++ cypress/e2e/center/local.cy.js
@@ -7,7 +7,7 @@ describe('Work with locals', () => {
     beforeEach(() => {
         cy.visit('https://center.umov.me/');
         //comando personalizado
-        cy.login('trindade', 'cypress', 'a')
+        cy.login('trindade', 'cypress', 'trocarsenha')
 
         cy.get(loc.MENU.OP_LOCATIONS).click()
 

4c3db10e01dd722ed926ae6db482d9119ffec4da
Antonio Trindade <atrindade@umov.me>
2024-03-26 20:08:52 -0300

    Add new tests local edit/inactive

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    
     setupNodeEvents(on, config) {
     },
     baseUrl: 'https://antoniotrindade.com.br/treinoautomacao',
+
   },
-  defaultCommandTimeout: 5000
+  defaultCommandTimeout: 5000,
+  video: false
 });
